6091a8a984891470ff22e299d3f6b55a4a4ac828
CAWilson94 <charlotteawilson.94@gmail.com>
2020-06-27 19:11:13 +0100

    flux example initial setup

---
+++ src/test/java/redditflux/RedditfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package redditflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
